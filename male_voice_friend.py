94700e2bb24f676f67b5e5dbe47b64a84ff4521b
cnriveraa <rivera.cn18@gmail.com>
2025-06-23 20:09:14 +0800

    Implement voice setup method in MaleVoiceFriend class for male voice properties

--- male_voice_friend.py
+++ male_voice_friend.py
@@ -3,3 +3,17 @@ import random
 
 class MaleVoiceFriend(VoiceFriend):
     '''Male voice friend implementation.'''
+
+    def _setup_voice(self):
+        '''Setup male voice properties.'''
+        voices = self.engine.getProperty('voices')
+        for voice in voices:
+            if "male" in voice.name.lower() or "david" in voice.name.lower():
+                self.engine.setProperty('voice', voice.id)
+                break
+        else:
+            self.engine.setProperty('voice', voices[0].id)  # Fallback to first available voice
+
+        # Set speaking rate and volume
+        self.engine.setProperty('rate', 150)
+        self.engine.setProperty('volume', 1.0)
