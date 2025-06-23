e60b76602f757f34db80fefdc3fbbd3b3dea52e1
cnriveraa <rivera.cn18@gmail.com>
2025-06-23 20:29:23 +0800

    Add special ability method to MaleVoiceFriend class for deep voice effect

--- male_voice_friend.py
+++ male_voice_friend.py
@@ -17,3 +17,13 @@ class MaleVoiceFriend(VoiceFriend):
         # Set speaking rate and volume
         self.engine.setProperty('rate', 150)
         self.engine.setProperty('volume', 1.0)
+
+    def special_ability(self):
+        '''Male's friend special ability.'''
+        self.say("Check this out! My deep voice can shake the room!")
+
+        # Deep voice effect temporarily
+        original_rate = self.engine.getProperty('rate')
+        self.engine.setProperty('rate', 50)  # Slower rate for deep voice effect
+        self.say("BOOM! Did you feel that? I can make the ground tremble with my voice!")
+        self.engine.setProperty('rate', original_rate)
