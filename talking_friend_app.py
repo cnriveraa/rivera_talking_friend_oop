51e7d4a36cd4c46f4ca4f3ede0a2bbee1de34a5d
cnriveraa <rivera.cn18@gmail.com>
2025-06-24 14:14:58 +0800

    Fix formatting and add newline at end of file in talking_friend_app.py

--- talking_friend_app.py
+++ talking_friend_app.py
@@ -2,3 +2,6 @@ import tkinter as tk
 from tkinter import ttk, messagebox, scrolledtext
 from male_voice_friend import MaleVoiceFriend
 from female_voice_friend import FemaleVoiceFriend
+
+class TalkingFriendApp:
+    '''Main Aplication GUI'''
