c24dd94d1184f72705f28893c4aff85af759795c
cnriveraa <rivera.cn18@gmail.com>
2025-06-24 14:19:45 +0800

    Refactor TalkingFriendApp initialization and layout setup

--- talking_friend_app.py
+++ talking_friend_app.py
@@ -5,3 +5,24 @@ from female_voice_friend import FemaleVoiceFriend
 
 class TalkingFriendApp:
     '''Main Aplication GUI'''
+
+    def __init__(self):
+        self.root = root
+        self.root.title = "Talking Friend"
+        self.root geometry('600x500')
+        self.root.protocol('WM_DELETE_WINDOW', self.on_close)
+
+        self.friend = None
+
+        # Main frames
+        self.control_frame = ttk.Frame(root, padding='10')
+        self.control_frame.pack(fill=tk.x)
+
+        self.chat_frame = ttk.Frame(root)
+        self.chat_frame.pack(fill=tk.BOTH, expand=True)
+
+        # Controls
+        self.setup_controls()
+
+        # Chat display
+        self.setup_chat()
