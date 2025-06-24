a734987206c2f94e7250a64870e5cd894b95c4dd
cnriveraa <rivera.cn18@gmail.com>
2025-06-24 14:07:08 +0800

    Add special ability method to FemaleVoiceFriend class for singing functionality

--- female_voice_friend.py
+++ female_voice_friend.py
@@ -20,3 +20,10 @@ class FemaleVoiceFriend(VoiceFriend):
         # Set female-typical voice properties
         self.engine.setProperty('rate', 170)
         self.engine.setProperty('volume', 0.8)
+
+    def special_ability(self):
+        '''Female friend's special ability'''
+        self.say("Let me sing you a little song!")
+        # Song: Soda Pop by Saja Boys (from the Netflix K-Animation film "KPop Demon Hunters".)
+        song = "You're all I can think of, Every drop I drink up, You're my soda pop, my little soda pop. Cool me down, you're so hot, Pour me up, I won't stop. You're my soda pop, my little soda pop."
+        self.say(song + "! ♫")
