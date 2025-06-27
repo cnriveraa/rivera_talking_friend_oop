9033bf4b517d69be8bfe438d400c77cf73c7ce69
cnriveraa <rivera.cn18@gmail.com>
2025-06-27 15:04:35 +0800

    Add main function in main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from talking_friend_app import TalkingFriendApp
 import tkinter as tk
 
-if __name__ == "__main__":
+def main():
     root = tk.Tk()
     app = TalkingFriendApp(root)
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
