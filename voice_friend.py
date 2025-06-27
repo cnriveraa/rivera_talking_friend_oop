db02c4bcf15092a16b98f20cb30ecf4d80e0859d
cnriveraa <rivera.cn18@gmail.com>
2025-06-28 01:35:27 +0800

    Remove type ignore comment for text_widget initialization

--- voice_friend.py
+++ voice_friend.py
@@ -9,7 +9,7 @@ class VoiceFriend(ABC):
 
     def __init__(self, text_widget, name):
         self.name = name
-        self.text_widget = text_widget # type: ignore
+        self.text_widget = text_widget 
         self.engine = pyttsx3.init()
         self._setup_voice()
         self._mood = "neutral" # default mood
